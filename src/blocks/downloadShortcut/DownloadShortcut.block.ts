3a5d3b826311162299a4fd491b012c270a73a8c0
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-30 19:20:21 -0300

    🔗 update: Change download link in DownloadShortcut block to point to LinkedIn profile for improved user engagement

--- src/blocks/downloadShortcut/DownloadShortcut.block.ts
+++ src/blocks/downloadShortcut/DownloadShortcut.block.ts
@@ -6,7 +6,7 @@ import { ButtonComponent } from "@/components/Button.component";
   standalone: true,
   imports: [ButtonComponent],
   template: `
-    <a href="https://www.google.com">
+    <a href="https://www.linkedin.com/in/ricardo-camilo-492b95b3/" target="_blank">
       <app-button
         [className]="'bg-red-500 text-white hover:bg-red-600'"
         [title]="'Download App'"
