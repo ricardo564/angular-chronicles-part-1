27399a4e149d0cccbf73d2baa6625d6c37effb89
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-30 12:43:38 -0300

    🎉 add: Update app configuration to include Angular Router with defined routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,12 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import {ApplicationConfig} from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app-routing.module';
 
 export const appConfig: ApplicationConfig = {
-  providers: [],
+  providers: [
+    provideRouter(routes)
+  ]
 };
