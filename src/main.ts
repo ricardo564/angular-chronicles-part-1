ca6ab5c6a52be082d7f62e9c8cfafd9545dadd31
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-30 12:33:06 -0300

    ✨ feat: Integrate Angular Router for application routing in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@
  */
 import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app-routing.module';
 
-bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
+bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport(), provideRouter(routes)]}).catch((err) =>
   console.error(err),
 );
