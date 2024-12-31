1e21ccad876273a00910fcc86e300b42f9770895
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-31 13:11:38 -0300

    🎉 add: Introduce FoodRatingCard component for displaying food items with ratings, images, and pricing, enhancing modularity and user experience

---
+++ src/components/FoodRatingCard.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from "@angular/core";
+import { ImageComponent } from "./Image.component";
+
+@Component({
+  selector: "app-food-rating-card",
+  standalone: true,
+  imports: [ImageComponent],
+  template: `
+    <div class="absolute inset-0 bg-transparent p-2  {{ className }}">
+      <div
+        class="flex items-center space-x-2 bg-white w-max p-1 border border-gray-100 rounded-lg shadow-lg mt-[3rem] lg:mt-[5rem] -ml-4 lg:-ml-9"
+      >
+        <app-image
+          [src]="imageSrc"
+          [alt]="name"
+          [className]="'w-12 h-12 rounded'"
+        />
+        <div>
+          <p class="font-medium">{{ name }}</p>
+          <div class="flex items-center space-x-2">
+            @for (star of stars; track star) {
+            <app-image
+              [src]="
+                star ? '/assets/svg/star.svg' : '/assets/svg/starEmpty.svg'
+              "
+              [alt]="star ? 'Star' : 'Star empty'"
+              [className]="'w-4 h-4'"
+            />
+            }
+          </div>
+          <p class="text-sm text-gray-600">{{ "$" + price }}</p>
+        </div>
+      </div>
+    </div>
+  `,
+})
+export class FoodRatingCardComponent {
+  @Input() imageSrc: string = "";
+  @Input() name: string = "";
+  @Input() rating: number = 0;
+  @Input() price: number = 0;
+  @Input() className: string = "";
+
+  get stars(): boolean[] {
+    return Array(5)
+      .fill(0)
+      .map((_, index) => index < this.rating);
+  }
+}
