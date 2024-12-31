7d684decfd404bcbf0d31e1e12c3c613eb8f0d42
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-31 20:20:04 -0300

    🎉 add: Integrate SecondSection component into home component for enhanced layout and content presentation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstSection } from '@/blocks/sections/First.section';
+import { SecondSection } from '@/blocks/sections/second.section';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CommonModule, FirstSection],
+  imports: [CommonModule, FirstSection, SecondSection],
   template: `
     <app-first-section></app-first-section>
+    <app-second-section></app-second-section>
   `
 })
 
