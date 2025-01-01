32e309610ce7fa82a916f5559f17801dbe10ab34
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-31 21:24:17 -0300

    🎨 update: Refactor MainLayout and Header components for improved layout consistency and responsiveness by adjusting max-width properties and cleaning up template formatting.

--- src/app/layouts/MainLayout.component.ts
+++ src/app/layouts/MainLayout.component.ts
@@ -1,27 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '@/components/Header.component';
-import { FooterComponent } from '@/blocks/sections/footer.section';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "@/components/Header.component";
+import { FooterComponent } from "@/blocks/sections/footer.section";
 
 @Component({
-  selector: 'app-main-layout',
+  selector: "app-main-layout",
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
     <div class="min-h-screen flex flex-col overflow-x-hidden">
       <app-header></app-header>
 
-      <main class="flex flex-col w-full mx-auto max-w-[1400px]">
+      <main
+        class="flex flex-col w-full mx-auto md:max-w-[900px] lg:max-w-[1400px]"
+      >
         <router-outlet></router-outlet>
       </main>
 
       <app-footer></app-footer>
     </div>
-  `
+  `,
 })
 
 export class MainLayoutComponent {}
--- src/components/Header.component.ts
+++ src/components/Header.component.ts
@@ -4,29 +4,33 @@ import { LogoComponent } from "@/components/Logo.component";
 import { DownloadShortcutBlock } from "@/blocks/downloadShortcut/DownloadShortcut.block";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
   imports: [NgFor, LogoComponent, DownloadShortcutBlock],
   template: `
-  <header class="max-w-[1400px] mx-auto flex justify-between items-center  px-4 py-4">
-    <app-logo></app-logo>
-    <nav class="hidden lg:flex space-x-8">
-      <a *ngFor="let item of menuItems"
-        [href]="item.link"
-        class="text-gray-700 hover:text-red-500 transition-colors">
-        {{item.label}}
-      </a>
-    </nav>
-    <app-download-shortcut></app-download-shortcut>
-  </header>
-  `
+    <header
+      class="md:max-w-[900px] lg:max-w-[1400px] mx-auto flex justify-between items-center  px-4 py-4"
+    >
+      <app-logo></app-logo>
+      <nav class="hidden lg:flex space-x-8">
+        <a
+          *ngFor="let item of menuItems"
+          [href]="item.link"
+          class="text-gray-700 hover:text-red-500 transition-colors"
+        >
+          {{ item.label }}
+        </a>
+      </nav>
+      <app-download-shortcut></app-download-shortcut>
+    </header>
+  `,
 })
 
 export class HeaderComponent {
   menuItems = [
-    { label: 'Today Special Offers', link: '#' },
-    { label: 'Why FoodHut', link: '#' },
-    { label: 'Our Menu', link: '#' },
-    { label: 'Our Popular Food', link: '#' }
+    { label: "Today Special Offers", link: "#" },
+    { label: "Why FoodHut", link: "#" },
+    { label: "Our Menu", link: "#" },
+    { label: "Our Popular Food", link: "#" },
   ];
 }
