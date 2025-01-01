bf3c5b602af4915ed989f4db2ea40ff7909e89fe
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-01 19:03:33 -0300

    🎉 add: Integrate AppSection component into HomeComponent to enhance the layout and provide additional content, improving overall user experience.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,17 +4,19 @@ import { IntroSection } from '@/blocks/sections/intro.section';
 import { SpecialOffersSection } from '@/blocks/sections/specialOffers.section';
 import { AboutUsSection } from '@/blocks/sections/aboutUs.section';
 import { MenuSection } from '@/blocks/sections/menu.section';
+import { AppSection } from '@/blocks/sections/app.section';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CommonModule, IntroSection, SpecialOffersSection, AboutUsSection, MenuSection],
+  imports: [CommonModule, IntroSection, SpecialOffersSection, AboutUsSection, MenuSection, AppSection],
   template: `
     <div class="grid grid-cols-1 gap-16">
       <intro-section></intro-section>
       <special-offers-section></special-offers-section>
       <about-us-section></about-us-section>
       <menu-section></menu-section>
+      <app-section></app-section>
     </div>
   `
 })
