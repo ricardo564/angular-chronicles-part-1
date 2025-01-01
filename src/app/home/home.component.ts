0d800f9ab425b68d734bbcc22964f49d10990558
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-01 16:55:37 -0300

    🎉 add: Integrate ThirdSection into Home component layout, enhancing the structure and responsiveness by wrapping sections in a grid for improved visual consistency.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstSection } from '@/blocks/sections/First.section';
 import { SecondSection } from '@/blocks/sections/second.section';
+import { ThirdSection } from '@/blocks/sections/thirdSection';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CommonModule, FirstSection, SecondSection],
+  imports: [CommonModule, FirstSection, SecondSection, ThirdSection],
   template: `
-    <app-first-section></app-first-section>
-    <app-second-section></app-second-section>
+    <div class="grid grid-cols-1 gap-16">
+      <app-first-section></app-first-section>
+      <app-second-section></app-second-section>
+      <app-third-section></app-third-section>
+    </div>
   `
 })
 
