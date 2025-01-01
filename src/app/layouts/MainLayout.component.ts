a332525c34128107ff1f0d5d01e457f35bc867ad
Ricardo Camilo <ricardo_hks@hotmail.com>
2024-12-31 21:48:55 -0300

    🎉 add: Integrate ImageComponent into MainLayout to display a black transparent background image, enhancing the visual design and layout consistency of the application.

--- src/app/layouts/MainLayout.component.ts
+++ src/app/layouts/MainLayout.component.ts
@@ -2,14 +2,21 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "@/components/Header.component";
 import { FooterComponent } from "@/blocks/sections/footer.section";
+import { ImageComponent } from "@/components/Image.component";
 
 @Component({
   selector: "app-main-layout",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ImageComponent],
   template: `
     <div class="min-h-screen flex flex-col overflow-x-hidden">
       <app-header></app-header>
+
+      <app-image
+        [src]="'assets/images/black-transparent-background.webp'"
+        [alt]="'Black transparent background'"
+        [className]="'min-w-screen w-full h-auto absolute inset-0 z-[-2]  -mt-24'"
+      ></app-image>
 
       <main
         class="flex flex-col w-full mx-auto md:max-w-[900px] lg:max-w-[1400px]"
