85cda25d668964d61a4a83b6bf449c8074737a80
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-01 19:22:51 -0300

    ✨ update: Refine Header component layout by removing redundant flex properties for cleaner structure and improved responsiveness.

--- src/components/Header.component.ts
+++ src/components/Header.component.ts
@@ -11,7 +11,7 @@ import { DownloadShortcutBlock } from "@/blocks/downloadShortcut/DownloadShortcu
     <header
       class="w-screen mx-auto flex justify-between items-center  px-4 py-4 fixed top-0 left-0 right-0 z-50  bg-gradient-to-b from-white via-white to-[#FFF9F3]/20"
     >
-      <div class="md:max-w-[900px] lg:max-w-[1400px] mx-auto flex justify-between items-center w-full flex justify-between">
+      <div class="md:max-w-[900px] lg:max-w-[1400px] mx-auto flex justify-between items-center w-full">
         <app-logo></app-logo>
         <nav class="hidden lg:flex space-x-8">
           <a
