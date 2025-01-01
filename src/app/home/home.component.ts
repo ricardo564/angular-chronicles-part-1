0c13dd6499b050675577c0e1ff222ed7619fc1dc
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-01 18:20:35 -0300

    🎉 add: Integrate MenuSection component into home layout for improved navigation and enhanced user experience.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FirstSection } from '@/blocks/sections/First.section';
 import { SecondSection } from '@/blocks/sections/second.section';
 import { ThirdSection } from '@/blocks/sections/thirdSection';
+import { MenuSection } from '@/blocks/sections/menu.section';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CommonModule, FirstSection, SecondSection, ThirdSection],
+  imports: [CommonModule, FirstSection, SecondSection, ThirdSection, MenuSection],
   template: `
     <div class="grid grid-cols-1 gap-16">
       <app-first-section></app-first-section>
       <app-second-section></app-second-section>
       <app-third-section></app-third-section>
+      <app-menu-section></app-menu-section>
     </div>
   `
 })
