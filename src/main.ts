f1733931f2b806690ee73279eebbc2cc08c3620b
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-02 10:14:56 -0300

    ✨ update: Integrate service worker support in main application bootstrap for improved performance and offline capabilities, enabling registration strategy based on application stability.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/pl
 import {AppComponent} from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
-bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport(), provideRouter(routes)]}).catch((err) =>
+bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport(), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]}).catch((err) =>
   console.error(err),
 );
