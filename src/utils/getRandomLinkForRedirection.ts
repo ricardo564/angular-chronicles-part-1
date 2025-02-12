be44390430e1b4e0cdfe1e0b4e3078f90c29d5a4
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-12 17:44:04 -0300

    🔧 update: Refresh random redirection links with updated personal profile URLs

--- src/utils/getRandomLinkForRedirection.ts
+++ src/utils/getRandomLinkForRedirection.ts
@@ -1,13 +1,7 @@
 export function getRandomLinkForRedirection() {
   const links = [
-    'https://resume.io/r/4bDKkHLB9',
-    'https://www.linkedin.com/in/ricardo-camilo-492b95b3/',
-    'https://recrutest.recrutei.com.br/public/result/eyJpdiI6IlFQRk9LZHlBelNOSVUzMGdQazd1RHc9PSIsInZhbHVlIjoiWUlVaU5HV0oxdHMxSmxCTjdEUkdVdz09IiwibWFjIjoiZGNjNTA5NWQ1OThlNmY5NGU2YmE5MjlmNjJhMzIxNDhiMGYxYTE0ZDQzZGRlODJhNzExMDk4MjFiMzRiZWM4NiJ9',
-    'https://x.com/Ricardo50993066',
-    'https://www.instagram.com/ricardohks/',
-    'https://www.figma.com/community/file/1103820487891554272',
-    'https://www.figma.com/@KamranAlime',
-    'https://x.com/KamranAlime',
+    'https://www.linkedin.com/in/ricardo-camilo-programador-frontend-web-developer/',
+    'https://www.instagram.com/ricardo.camilo.dev/',
   ];
 
   return links[Math.floor(Math.random() * links.length)];
