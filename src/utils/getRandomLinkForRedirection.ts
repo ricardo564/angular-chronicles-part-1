325a084cc36242a3fd50e3f22fcfb610c6b58d92
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-12 17:44:47 -0300

    🔗 update: Add sitemap.xml link to random redirection options

--- src/utils/getRandomLinkForRedirection.ts
+++ src/utils/getRandomLinkForRedirection.ts
@@ -2,6 +2,7 @@ export function getRandomLinkForRedirection() {
   const links = [
     'https://www.linkedin.com/in/ricardo-camilo-programador-frontend-web-developer/',
     'https://www.instagram.com/ricardo.camilo.dev/',
+    'https://persona-nextjs-chronicles-part-2.netlify.app/sitemap.xml',
   ];
 
   return links[Math.floor(Math.random() * links.length)];
